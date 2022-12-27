ab23a1aa9181b71bdd9a29907393f4cebcbe8d0c
au <auau0197@gmail.com>
2022-12-27 16:34:39 +0700

    finish

--- lab10_1.cpp
+++ lab10_1.cpp
@@ -2,11 +2,16 @@
 #include<iomanip> //For using setw(), setprecision(), ...
 using namespace std;
 
-int main(){	
+int main(){
+	int i = 1;
+    double loan, rate, pay, New, Prev, inte, Tot;
 	cout << "Enter initial loan: ";
+	cin >> loan;
 	cout << "Enter interest rate per year (%): ";
+	cin >> rate;
 	cout << "Enter amount you can pay per year: ";
-
+    cin >> pay;
+    
 	//use 'setw' to set width of table and 'left' to set left-alignment
 	//you can change input argument of 'setw()' to see the effect
 	//Try to change from 'left' to 'right' and see the effect
@@ -21,13 +26,26 @@ int main(){
 	//use 'fixed' and 'setprecision' to fix the number of decimal digits for displaying
 	//you can change input argument of 'setprecision()' to see the effect
 	cout << fixed << setprecision(2); 
-	cout << setw(13) << left << 1; 
-	cout << setw(13) << left << 1000.0;
-	cout << setw(13) << left << 50.0;
-	cout << setw(13) << left << 1050.0;
-	cout << setw(13) << left << 100.0;
-	cout << setw(13) << left << 950.0;
-	cout << "\n";	
+	Prev = loan;
+	New = loan;
+	for(i = 1; New != 0; i++){
+		inte = Prev*(rate/100);
+		Tot = inte+Prev;
+		if(Tot < pay){
+			pay = Tot;
+		}
+		New = Tot - pay;
+
+		cout << setw(13) << left << i; 
+		cout << setw(13) << left << Prev;
+		cout << setw(13) << left << inte;
+		cout << setw(13) << left << Tot;
+		cout << setw(13) << left << pay;
+		cout << setw(13) << left << New;
+		cout << "\n";	
+		Prev = New;
+	}
+	
 	
 	return 0;
 }
